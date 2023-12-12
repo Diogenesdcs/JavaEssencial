f8198a18e8372c688a5741f2fcdb38c3d721e612
Diogenesdcs <107212403+Diogenesdcs@users.noreply.github.com>
2023-12-11 23:03:31 -0300

    Criação de  programa java encapsulamento

--- src/br/com/geekuniversit/secao11/Conta.java
+++ src/br/com/geekuniversit/secao11/Conta.java
@@ -1,26 +1,45 @@
 package br.com.geekuniversit.secao11;
 
-public class Conta {
-    int numero;
-    float saldo;
-    float limite;
-    Cliente cliente; //como é uma classe criada então podemos  utilizar como parametro de outra classe.
+    //classe
+    public class Conta {
+        int numero;
+        private float saldo; //variavel privada
+        private float limite;
+        Cliente cliente; //como é uma classe criada então podemos  utilizar como parametro de outra classe.
 
-    //construtor
-    public Conta(int numero, float saldo, float limite, Cliente cliente) {
-        this.numero = numero;
-        this.saldo = saldo;
-        this.limite = limite;
-        this.cliente = cliente;
+        //construtor
+        public Conta(int numero, float saldo, float limite, Cliente cliente) {
+            this.numero = numero;
+            this.saldo = saldo;
+            this.limite = limite;
+            this.cliente = cliente;
+        }
 
+        //métodos da classe
+        //void nome_da_função (declaração_de_parâmetros);
+        public void sacar(float valor) {
+            if (valor <= this.saldo) {
+                this.saldo = this.saldo - valor;
+                //saldo é igual  a saldo mas o limite.
+            } else if (valor <= (this.saldo + this.limite)) {
+
+                float val_ret = this.saldo - valor; //calculo do valor excedente do saque
+                if (val_ret < 0){
+                    this.saldo = 0;
+            }
+            val_ret = this.limite + val_ret;
+            this.limite = val_ret;
+        }else {
+            System.out.println("Saldo insuficiente");
+        }
     }
 
-//void nome_da_função (declaração_de_parâmetros);
-    void sacar(float valor) {
-        this.saldo = this.saldo - valor;  //saldo é igual  a saldo menos o valor.
-    }
-
-    void depositar(float valor) {
+    public void depositar( float valor){
         this.saldo = this.saldo + valor;
+        }
+
+        //Método getter do atributo Saldo
+    public float getsaldo() {
+        return this.saldo + this.limite;
+        }
     }
-}
--- src/br/com/geekuniversit/secao11/Programa25.java
+++ src/br/com/geekuniversit/secao11/Programa25.java
@@ -5,11 +5,15 @@ public class Programa25 {
         Cliente joao = new Cliente("joao da Silva", "Rua da Paz,  45"); //instância da classe Cliente.
         Cliente maria = new Cliente("Maria da Silva", "Rua da Paz,  45");
 
-        Conta conta_joao = new Conta(1, 100.0f, 200.0f, joao);//instância da classe Conta
+        Conta conta_joao = new Conta(1, 100.0f, 100.0f, joao);//instância da classe Conta
         Conta conta_maria = new Conta(1, 300.0f, 500.0f, maria);
 
-        System.out.println("Saldo do João: " + conta_joao.saldo);
-        System.out.println("Saldo do Maria: " + conta_maria.saldo);
+        System.out.println("Saldo do João: " + conta_joao.getsaldo());
+        System.out.println("Saldo do Maria: " + conta_maria.getsaldo());
+
+        conta_joao.sacar(300);
+        System.out.println("Saldo do João: (Depois do saque)" + conta_joao.getsaldo());
+
     }
 
 
