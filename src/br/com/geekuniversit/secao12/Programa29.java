85ab234f9c0c5315b4b0507872db26670a42edcd
Diogenesdcs <107212403+Diogenesdcs@users.noreply.github.com>
2024-01-07 23:05:16 -0300

    //overloading

--- src/br/com/geekuniversit/secao12/Programa29.java
+++ src/br/com/geekuniversit/secao12/Programa29.java
@@ -2,7 +2,7 @@ package br.com.geekuniversit.secao12;
 
  //sobrecarga de métodos.
  //overloading
- //só acontece quando o mesmo metodo utilizado tem parametros diferentes
+ //só acontece quando o mesmo metodo utilizado tem parâmetros diferentes.
 public class Programa29 {
     public static void main(String[] args) {
         Pessoa p1 = new Pessoa();
