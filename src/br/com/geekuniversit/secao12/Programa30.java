eb729ebae34b8044ae456fa0718db9b8c96d98ea
Diogenesdcs <107212403+Diogenesdcs@users.noreply.github.com>
2024-01-07 23:04:40 -0300

    //sobescrita de metodo ou overriding

---
+++ src/br/com/geekuniversit/secao12/Programa30.java
@@ -0,0 +1,10 @@
+package br.com.geekuniversit.secao12;
+//sobescrita de metodo ou overriding
+/*
+É o ato de sobescrevermos metódos da classe pai em classe filhas
+ */
+public class Programa30 {
+    public static void main(String[] args) {
+
+    }
+}
