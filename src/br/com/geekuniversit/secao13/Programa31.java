c4b5d49249cba63158c1bbbd5befff7837a24762
Diogenesdcs <107212403+Diogenesdcs@users.noreply.github.com>
2024-01-09 22:23:00 -0300

    Classes abstratas.

---
+++ src/br/com/geekuniversit/secao13/Programa31.java
@@ -0,0 +1,16 @@
+package br.com.geekuniversit.secao13;
+//classes abstratas
+
+
+/*Uma classe abstrata em Java é uma classe que não pode ser instanciada diretamente.
+Ela é geralmente usada como uma classe base para outras classes (subclasses) que estendem suas funcionalidades.
+ Uma classe abstrata pode conter métodos abstratos (métodos sem implementação) e métodos concretos (métodos com implementação).
+ */
+
+import br.com.geekuniversit.secao12.Pessoa;
+
+public class Programa31 {
+    public static void main(String[] args) {
+
+    }
+}
