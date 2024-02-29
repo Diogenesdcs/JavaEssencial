014547b78608abab79eca23b457183ea8ed12346
Diogenesdcs <107212403+Diogenesdcs@users.noreply.github.com>
2024-02-28 22:17:58 -0300

    match

---
+++ src/br/com/geekuniversit/secao18/programa40.java
@@ -0,0 +1,16 @@
+package br.com.geekuniversit.secao18;
+
+//Realizando cálculos com Math
+//A biblioteca math possui vários métodos e constantes estáticas
+//para que possamos utilizar nos nossos programas.
+
+public class programa40 {
+    public static void main(String[] args) {
+        System.out.println(Math.E); //constante
+        System.out.println(Math.PI);
+        System.out.println(Math.sin(45));
+        System.out.println(Math.cos(45));
+        System.out.println(Math.tan(45));
+
+    }
+}
